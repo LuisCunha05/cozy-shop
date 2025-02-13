67aa90618e78a907d48ab945f762780500c6d0b3
LuisCunha05 <luisf.cunha05@gmail.com>
2025-02-13 11:11:28 -0400

    adicionado gerenciamento dos favoritos no localStorage

--- src/js/favoritos.js
+++ src/js/favoritos.js
@@ -1,1 +1,50 @@
-"hehe"
+
+/**
+ * Coloca o array dentro dos favoritos
+ * @param {string[]} arr 
+ * @returns 
+ */
+const setFavorito = arr => localStorage.setItem('favoritos', JSON.stringify(arr))
+
+/**
+ * Retorna lista de IDs dos produtos favoritos
+ * @returns {string[]}
+ */
+const visualizarFavorito = () => {
+    return JSON.parse(localStorage.getItem('favoritos')) || []
+}
+
+/**
+ * Adiciona um novo favorito. Adicionado no localStorage
+ * @param {string} id 
+ */
+const adicionarFavorito = (id) => {
+    /**
+     * @type {string[]}
+     */
+    const favoritos = visualizarFavorito()
+    
+    if(favoritos.includes(id)) return
+
+    favoritos.push(id)
+
+    setFavorito(favoritos)
+}
+
+/**
+ * Remove o produto dos favoritos caso ele exista
+ * @param {string} id 
+ */
+const removerFavorito = (id) => {
+    const favoritos = visualizarFavorito()
+
+    if(!favoritos.includes(id)) return
+
+    console.log('remove before: ', favoritos)
+    favoritos.splice(favoritos.indexOf(id), 1)
+    console.log('remove after: ', favoritos)
+
+    setFavorito(favoritos)
+}
+
+
