ed6349e40bbcc9940ee3e24c125084cd927f7055
PauloMatheusBS <paulomatheus7294@hotmail.com>
2025-02-19 18:04:26 -0400

    conectada home com pagina produto criada

--- src/js/home.js
+++ src/js/home.js
@@ -1,29 +1,27 @@
-import componenteCategoria from '../componentes/componenteCategoria.js';
-import componenteProduto from '../componentes/componenteProduto.js';
+import componenteCategoria from "../componentes/componenteCategoria.js";
+import componenteProduto from "../componentes/componenteProduto.js";
 
 const productList = document.getElementById("product-list");
 const categoryContainer = document.getElementById("category-container");
 const categorySelect = document.getElementById("category-select");
 
 async function loadCategories() {
-    const response = await fetch("https://fakestoreapi.com/products/categories");
-    const categories = await response.json();
-  
-    const categoryHtml = componenteCategoria({
-      categories,
-      onSelectCategory: (event) => loadProducts(event.target.value),
-    });
-    
-    categoryContainer.innerHTML = categoryHtml;
-  
-   
-    const categorySelect = document.getElementById("category-select");
-    categorySelect.addEventListener("change", (event) => {
-      const category = event.target.value;
-      loadProducts(category);
-    });
-  }
-  
+  const response = await fetch("https://fakestoreapi.com/products/categories");
+  const categories = await response.json();
+
+  const categoryHtml = componenteCategoria({
+    categories,
+    onSelectCategory: (event) => loadProducts(event.target.value),
+  });
+
+  categoryContainer.innerHTML = categoryHtml;
+
+  const categorySelect = document.getElementById("category-select");
+  categorySelect.addEventListener("change", (event) => {
+    const category = event.target.value;
+    loadProducts(category);
+  });
+}
 
 async function loadProducts(category = "") {
   let url = "https://fakestoreapi.com/products";
@@ -42,14 +40,20 @@ async function loadProducts(category = "") {
     noProductsMessage.textContent = "Nenhum produto encontrado.";
     productList.appendChild(noProductsMessage);
   } else {
-    
     products.forEach((product) => {
-      const productHtml = componenteProduto({
-        title: product.title,
-        image: product.image,
-        price: product.price,
-        id: product.id
-      });
+      const productHtml = `
+        <div class="produto">
+          <a href="./produto.html?id=${product.id}">
+            <div class="imgProduto">
+              <img src="${product.image}" alt="${product.title}">
+            </div>
+            <div class="infoProduto">
+              <h2>${product.title}</h2>
+              <p class="preco">R$ ${product.price}</p>
+            </div>
+          </a>
+        </div>
+      `;
 
       productList.innerHTML += productHtml;
     });
@@ -58,10 +62,8 @@ async function loadProducts(category = "") {
 
 document.addEventListener("DOMContentLoaded", () => {
   loadCategories();
-  loadProducts(); 
+  loadProducts();
 });
-
-
 
 categorySelect.addEventListener("change", (event) => {
   const category = event.target.value;
