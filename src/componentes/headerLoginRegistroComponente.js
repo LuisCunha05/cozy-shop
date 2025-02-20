7c57a799a95a589fb8cb3fecdaecd83b517e71ee
Luis Fernando <luisvirissimo789@gmail.com>
2025-02-20 07:24:42 -0400

    Pronto

---
+++ src/componentes/headerLoginRegistroComponente.js
@@ -0,0 +1,18 @@
+const headerLoginRegistroComponente = () => {
+    return `
+    <header class="header">
+            <img src="../images/logo.svg" alt="Logo" class="header-image">
+            <div class="header-buttons">
+                <a href="home.html" class="header-button">Home</a>
+                <a href="produtos.html" class="header-button">Produtos</a>
+                <a href="editar_produtos.html" class="header-button">Editar Produtos</a>
+                <a href="usuario.html" class="header-button">Perfil</a>
+                <a href="favoritos.html" class="header-button">Favoritos</a>
+                <a href="carrinho.html" class="header-button">Carrinho</a>
+                <a href="cadastro.html" class="header-button">Cadastro</a>
+                <a href="login.html" class="header-button">Login</a>
+            </div>
+        </header>`;
+};
+
+export default headerLoginRegistroComponente;
--- src/js/base.js
+++ src/js/base.js
@@ -1,5 +1,8 @@
 import headerComponente from "../componentes/headerComponente.js";
+import headerLoginRegistroComponente from "../componentes/headerLoginRegistroComponente.js";
 
+document.getElementById("header").innerHTML += headerComponente();
+document.getElementById("headerLoginRegistro").innerHTML += headerLoginRegistroComponente();
 const element = document.getElementById("header")
 
 if(element && element.innerText.length === 0) {
